a8b68970594a8f7df5ee28e38ae8e7eb7126d456
David Llamas <david16llamas@gmail.com>
2026-03-24 18:38:49 +0100

    Afegit component 'TargetaPelicula' amb HTML, incloent la gestió de dades de 'Pelicula'

--- src/app/components/targeta-pelicula/targeta-pelicula.component.ts
+++ src/app/components/targeta-pelicula/targeta-pelicula.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Pelicula } from '../../models/pelicula.model';
 
 @Component({
   selector: 'app-targeta-pelicula',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './targeta-pelicula.component.html',
   styleUrl: './targeta-pelicula.component.scss'
 })
 export class TargetaPeliculaComponent {
-
+  @Input({required: true}) pelicula!: Pelicula;
+  
 }
