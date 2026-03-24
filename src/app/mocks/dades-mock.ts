b942a6c6b72de368165363bc2db0ecd118d34dc0
David Llamas <david16llamas@gmail.com>
2026-03-24 18:09:12 +0100

    Afegit model de dades 'Pelicula' i mock de pel·lícules

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,94 @@
+import { Pelicula } from "../models/pelicula.model";
+
+export const PELICULES_MOCK: Pelicula[] = [
+  {
+    id: 1,
+    titol: "Origen",
+    director: "Christopher Nolan",
+    any: 2010,
+    durada: 148,
+    sinopsis: "En Dom Cobb (Leonardo DiCaprio) és el millor en l'art de l'extracció, és a dir, en aconseguir els secrets del subconscient just en el moment en què la ment de la víctima és més vulnerable: mentre dorm, en Cobb, juntament amb l'Arthur (Joseph Gordon-Levitt), l'Eames (Tom Hardy) i l'Ariadna (Elliot Page), hauran d'infiltrar-se en la ment d'en Fischer (Cillian Murphy), el nou director d'una gran empresa, per implantar-hi la idea d'acabar amb ella.",
+    cartell: "https://www.themoviedb.org/t/p/w300_and_h450_face/mJfTycNg8PxjvxzpnKnzAAG7RIP.jpg"
+  },
+  {
+    id: 2,
+    titol: "Interestel·lar",
+    director: "Christopher Nolan",
+    any: 2014,
+    durada: 169,
+    sinopsis: "Un grup d’exploradors fa ús d’un forat de cuc descobert recentment per superar les limitacions dels viatges espacials tripulats i vèncer les immenses distàncies que comporta un viatge interestel·lar.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/iawqQdFKI7yTUoSkDNP8gyV3J3r.jpg"
+  },
+  {
+    id: 3,
+    titol: "El cavaller fosc",
+    director: "Christopher Nolan",
+    any: 2008,
+    durada: 152,
+    sinopsis: "Batman/Bruce Wayne torna per continuar la seva guerra contra el crim. Amb l’ajuda del tinent Jim Gordon i del fiscal del districte Harvey Dent, Batman es proposa destruir el crim organitzat a la ciutat de Gotham. El triumvirat demostra la seva eficàcia, però, de sobte, apareix el Joker, un nou criminal que desencadena el caos i té aterrits els ciutadans.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/8QDQExnfNFOtabLDKqfDQuHDsIg.jpg"
+  },
+  {
+    id: 4,
+    titol: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    any: 1994,
+    durada: 154,
+    sinopsis: "Jules i Vincent, dos assassins a sou amb molt poques llums, treballen per a Marsellus Wallace. Vincent li confessa a Jules que Marsellus li ha demanat que tingui cura de Mia, la seva dona. Jules li recomana prudència perquè és molt perillós sobrepassar-se amb la nòvia del cap. Quan arriba l’hora de treballar, tots dos s’han de posar mans a l’obra. La seva missió: recuperar un misteriós maletí.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/hNcQAuquJxTxl2fJFs1R42DrWcf.jpg"
+  },
+  {
+    id: 5,
+    titol: "Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+    any: 1999,
+    durada: 136,
+    sinopsis: "Thomas Anderson porta una doble vida: de dia és programador en una important empresa de programari, i de nit és un hacker informàtic anomenat Neo. La seva vida no tornarà a ser igual quan uns misteriosos personatges el convidin a descobrir la resposta a la pregunta que no el deixa dormir: què és Matrix?",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/ejmTPNAkgZaVJ4AI9zb9SehAYU0.jpg"
+  },
+  {
+    id: 6,
+    titol: "Gladiator",
+    director: "Ridley Scott",
+    any: 2000,
+    durada: 155,
+    sinopsis: "L’any 180, l’Imperi Romà domina tot el món conegut. Després d’una gran victòria sobre els bàrbars del nord, l’ancià emperador Marc Aureli decideix transferir el poder a Màxim, valent general dels seus exèrcits i home d’inquebrantable lleialtat a l’imperi. Però el seu fill Còmode, que aspirava al tron, no ho accepta i intenta assassinar Màxim.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/90QFOG5zSN4cbrIVs4DL4ePAuA5.jpg"
+  },
+  {
+    id: 7,
+    titol: "El Senyor dels Anells: La Germandat de l'Anell",
+    director: "Peter Jackson",
+    any: 2001,
+    durada: 178,
+    sinopsis: "A la Terra Mitjana, el Senyor Fosc Sàuron va crear els Grans Anells del Poder, forjats pels ferrers elfs. Tres per als reis elfs, set per als senyors nans, i nou per als homes mortals. Secretament, en Sàuron també va forjar un anell mestre, l'Anell Únic, que conté dins seu el poder per esclavitzar tota la Terra Mitjana. Amb l'ajuda d'un grup d'amics i aliats valerosos, en Frodo emprèn un viatge perillós amb la missió de destruir l'Anell Únic. Però en Sàuron envia els seus servidors per perseguir el grup. Si en Sàuron aconseguís recuperar l'Anell, seria la fi de la llibertat a la Terra Mitjana.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/9xtH1RmAzQ0rrMBNUMXstb2s3er.jpg"
+  },
+  {
+    id: 8,
+    titol: "El club de la lluita",
+    director: "David Fincher",
+    any: 1999,
+    durada: 139,
+    sinopsis: "Un home consumit per l’insomni i una vida sense sentit coneix en Tyler Durden, un carismàtic anarquista que el porta a un club secret on el dolor desperta alguna cosa adormida. Allò que comença com una liberació aviat es converteix en una guerra contra el sistema…",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/bI0BhpswEApC0B17RhLZtRVPZiQ.jpg"
+  },
+  {
+    id: 9,
+    titol: "Forrest Gump",
+    director: "Robert Zemeckis",
+    any: 1994,
+    durada: 142,
+    sinopsis: "Forrest Gump és un noi amb discapacitat intel·lectual lleu i alguna dificultat motora que, malgrat tot, arribarà a convertir-se, entre altres coses, en un heroi durant la Guerra del Vietnam. La seva persistència i bondat el portaran a aconseguir una gran fortuna, a ser objecte del clam popular i a relacionar-se amb les més altes esferes socials i polítiques del país, sempre sense oblidar la Jenny, el seu gran amor des de la infantesa.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/oiqKEhEfxl9knzWXvWecJKN3aj6.jpg"
+  },
+  {
+    id: 10,
+    titol: "Avatar",
+    director: "James Cameron",
+    any: 2009,
+    durada: 162,
+    sinopsis: "Any 2154. Jake Sully, un exmarine en cadira de rodes, és enviat al planeta Pandora, on s’ha creat el programa Avatar, gràcies al qual els éssers humans poden controlar de forma remota un cos biològic amb aparença i genètica de l’espècie nativa. Aviat es trobarà davant la cruïlla entre seguir les ordres dels seus superiors o defensar el món que l’ha acollit i que sent com a seu.",
+    cartell: "https://www.themoviedb.org/t/p/w600_and_h900_face/t5T3LPbLLgP2OP6kloM9p2PXpJL.jpg"
+  }
+];
---
+++ src/app/models/pelicula.model.ts
@@ -0,0 +1,9 @@
+export interface Pelicula {
+  id: number;
+  titol: string;
+  director: string;
+  any: number;
+  durada: number;
+  sinopsis: string;
+  cartell: string;
+}
