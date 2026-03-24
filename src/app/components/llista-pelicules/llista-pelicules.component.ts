ae525d88f380b0b8af3e61014cb19126eb03dcc2
David Llamas <david16llamas@gmail.com>
2026-03-24 18:34:16 +0100

    Afegit components 'BarraCerca', 'LlistaPelicules' i 'TargetaPelicula' amb HTML, SCSS i tests

---
+++ src/app/components/llista-pelicules/llista-pelicules.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llista-pelicules',
+  standalone: true,
+  imports: [],
+  templateUrl: './llista-pelicules.component.html',
+  styleUrl: './llista-pelicules.component.scss'
+})
+export class LlistaPeliculesComponent {
+
+}
---
+++ src/app/components/targeta-pelicula/targeta-pelicula.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-targeta-pelicula',
+  standalone: true,
+  imports: [],
+  templateUrl: './targeta-pelicula.component.html',
+  styleUrl: './targeta-pelicula.component.scss'
+})
+export class TargetaPeliculaComponent {
+
+}
