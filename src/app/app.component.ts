87d73adeedaecb0a9e13be7fcb278146371c410e
David Llamas <david16llamas@gmail.com>
2026-03-24 19:01:56 +0100

    Afegit component 'BarraCerca' amb funcionalitat de cerca i esborrat al formulari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { TargetaPeliculaComponent } from './components/targeta-pelicula/targeta-pelicula.component';
 import { LlistaPeliculesComponent } from './components/llista-pelicules/llista-pelicules.component';
 import { Pelicula } from './models/pelicula.model';
 import { PELICULES_MOCK } from './mocks/dades-mock';
+import { BarraCercaComponent } from './components/barra-cerca/barra-cerca.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TargetaPeliculaComponent, LlistaPeliculesComponent],
+  imports: [RouterOutlet, TargetaPeliculaComponent, LlistaPeliculesComponent, BarraCercaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
