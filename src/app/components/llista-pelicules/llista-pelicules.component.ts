47bf7dc77a6f09761430f3ba476436a2a9f71bf8
David Llamas <david16llamas@gmail.com>
2026-03-24 18:52:01 +0100

    Afegit component 'LlistaPelicules' amb gestió de dades de 'Pelicula' i estructura HTML

--- src/app/components/llista-pelicules/llista-pelicules.component.ts
+++ src/app/components/llista-pelicules/llista-pelicules.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Pelicula } from '../../models/pelicula.model';
+import { TargetaPeliculaComponent } from '../targeta-pelicula/targeta-pelicula.component';
 
 @Component({
   selector: 'app-llista-pelicules',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TargetaPeliculaComponent],
   templateUrl: './llista-pelicules.component.html',
   styleUrl: './llista-pelicules.component.scss'
 })
 export class LlistaPeliculesComponent {
+  @Input({required: true}) pelicules!: Pelicula[];
 
+  trackById(index: number, pelicula: Pelicula): number {
+    return pelicula.id;
+  }
 }
