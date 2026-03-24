7475fec032b535f60d47a1cfd7a40c221d3950eb
David Llamas <david16llamas@gmail.com>
2026-03-24 18:58:21 +0100

    Afegit funcionalitat de cerca i per esborrar el camp de cerca al component 'BarraCerca'

--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './barra-cerca.component.html',
   styleUrl: './barra-cerca.component.scss'
 })
 export class BarraCercaComponent {
+  @Output() cercaCanviada = new EventEmitter<string>();
 
-}
+  textCerca: string = '';
+  
+  cercar() {
+    if (this.textCerca.length >= 3) {
+      this.cercaCanviada.emit(this.textCerca);
+    }
+  }
+
+  netejar(): void {
+    this.textCerca = '';
+    this.cercaCanviada.emit('');
+  }
+} 
