1e3e3d9631fb5537d059f6a93ae3782f676a5631
David Llamas <david16llamas@gmail.com>
2026-03-24 18:52:24 +0100

    Afegit importacions de components 'TargetaPelicula' i 'LlistaPelicules' a 'AppComponent' i inicialització de dades de 'Pelicula'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TargetaPeliculaComponent } from './components/targeta-pelicula/targeta-pelicula.component';
+import { LlistaPeliculesComponent } from './components/llista-pelicules/llista-pelicules.component';
+import { Pelicula } from './models/pelicula.model';
+import { PELICULES_MOCK } from './mocks/dades-mock';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TargetaPeliculaComponent, LlistaPeliculesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ioc-angular-filmateca-david';
+  pelicules: Pelicula[] = PELICULES_MOCK
 }
