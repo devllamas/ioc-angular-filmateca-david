817cb19e1b37fc972284fbe699e7f4bef4936ed1
David Llamas <david16llamas@gmail.com>
2026-05-05 20:26:50 +0200

    Sistema de rutes configurat. Components basics creats. navegació funcional amb menú actiu implementada.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SearchComponent } from './features/search/search.component';
+import { DetailComponent } from './features/detail/detail.component';
+import { LoginComponent } from './features/login/login.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'item/:id', component: DetailComponent },
+  { path: 'login', component: LoginComponent },
+  //{
+      //path: 'favorites',
+    //loadChildren: () => import('./features/favorites/favorites.module').then(m => m.FavoritesModule)
+  //},
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
