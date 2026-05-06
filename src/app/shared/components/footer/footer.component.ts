f19ee88131f726240fd5596664f073c04ac99cfc
David Llamas <david16llamas@gmail.com>
2026-05-06 11:43:07 +0200

    estil del footer actualitzat

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  // Optimització per a components estàtics
+  changeDetection: ChangeDetectionStrategy.OnPush 
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
